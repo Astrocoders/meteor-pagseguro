e3c666832f8f06bd90ebf2bc5b2f33e8ab1fce6f
Yuri Jean Fabris <yuri.fabris@gmail.com>
2016-06-24 01:39:39 -0300

    Set Iron Router to work only on server

--- package.js
+++ package.js
@@ -15,10 +15,13 @@ Package.onUse(function(api) {
     'mongo@1.1.0',
     'frozeman:persistent-minimongo@0.1.4',
     'peerlibrary:xml2js@0.4.8_1',
-    'iron:router@1.0.8',
     'http@1.1.0',
     'random'
   ]);
+  
+  api.use([
+    'iron:router@1.0.8',
+  ], 'server');
 
   api.addFiles('lib/pagseguro.js', ['client', 'server']);
   api.addFiles('server/pagseguro-utils.js', ['server']);
@@ -32,15 +35,5 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('gbit:faker@0.0.3');
-  api.use('gbit:pagseguro');
-  api.use('frozeman:persistent-minimongo');
-  api.use('xolvio:http-interceptor@0.5.1');
-  api.use('iron:router');
-  api.use('random');
   
-  api.addFiles('tests/_preps.js', ['client', 'server']);
-  api.addFiles('tests/pagseguro-server.js', ['server']);
-  api.addFiles('tests/pagseguro-client.js', ['client']);
 });
