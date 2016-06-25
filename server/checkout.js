353a91a6207a926aabdfed9dafb1a50649a57365
Gabriel Rubens <grsabreu@gmail.com>
2016-06-24 22:36:18 -0300

    Create PagSeguro.checkout

---
+++ server/checkout.js
@@ -0,0 +1,46 @@
+PagSeguro.createPurchase = ({sender, items, shippingAddress}) => {
+  return {
+    normalizeAddressObj(shippingAddress){
+      return _.mapKeys(shippingAddress, (v, key) => `shippingAddress${_.capitalize(key)}`);
+    },
+
+    getPurchase(){
+      return {
+        reference: Random.id(),
+        items,
+        createdAt: new Date(),
+        sender: _.mapKeys(sender, (v, key) => `sender${_.capitalize(key)}`),
+        shippingAddress: this.normalizeAddressObj(shippingAddress),
+      };
+    },
+
+    getRequest(purchase:object){
+      PagSeguroUtils.serializeRequest(purchase);
+    },
+    
+    getCheckoutCode(request:object, onError){
+      const request = HTTP.post(PagSeguro.config.API_URL, {
+        params: request,
+        headers: {
+          'Accept-Charset': PagSeguro.config.encoding,
+        },
+      });
+
+      if (response.error) {
+        onError(response.error, response);
+        return false;
+      }
+
+      return xml2js.parseStringSync(response.content).checkout.code;
+    },
+
+    getCheckoutURL({ onError }){
+      const purchaseAsRequest = this.getRequest(this.getPurchase());
+      const checkoutCode = this.getCheckoutCode(purchaseAsRequest, onError);
+
+      if(!checkoutCode) return false;
+
+      return `${PagSeguro.config.API_PAYMENT_URL}${checkoutCode}`;
+    },
+  };
+}
