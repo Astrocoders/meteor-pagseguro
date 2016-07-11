2f5f05a01dcd511f6b997e7763339690ebbdbf79
Gabriel Rubens <grsabreu@gmail.com>
2016-07-11 00:38:01 -0300

    Update postback to use Picker

--- server/postback.js
+++ server/postback.js
@@ -1,7 +1,32 @@
 Meteor.startup(function() {
-  Router.route(
-    PagSeguro.config.cbUrls.notifications,
-    PagSeguro.config._notificationHandler,
-    {where: 'server'},
-  );
+  const bodyParser = Npm.require('body-parser');
+
+  Picker.middleware( bodyParser.json() );
+  Picker.middleware( bodyParser.urlencoded( { extended: false } ) );
+
+  Picker.route(PagSeguro.config.cbUrls.notifications, function(params, req, res, next){
+    console.log('[PagSeguro]', 'Receiving a postback');
+    console.log('[PagSeguro]', 'And this is the request body', req.body);
+
+    const data = req.body;
+
+    if(!data) {
+      res.end();
+      return;
+    }
+
+    const response = HTTP.get(PagSeguro.config.API_NOTIFICATION_URL + data.notificationCode, {
+      params: {
+        email: PagSeguro._settings.email,
+        token: PagSeguro._settings.token,
+      },
+    });
+
+    if (response.statusCode !== 200) {
+      return;
+    }
+
+    const { transaction } = xml2js.parseStringSync(response.content); 
+    PagSeguro.config._notificationHandler(transaction);
+  });
 });
